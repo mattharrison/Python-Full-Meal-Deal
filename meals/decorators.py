3bc1b879926a85301ef3eefa77b69d296a72c23b
matt <matthewharrison@gmail.com>
2010-10-06 22:00:44 -0600

    added decorators.py meal

---
+++ meals/decorators.py
@@ -0,0 +1,59 @@
+import StringIO
+import sys
+import types
+import unittest
+
+class TestDecorators(unittest.TestCase):
+    def test_decorators(self):
+        # Decorators
+        # create a decorator, ``noisy`` that
+        # prints "before" before invoking the
+        # wrapped function and "after", after
+        # invoking it
+        # ================================
+        
+        @noisy
+        def nothing():
+            pass
+
+        old_stdout = sys.stdout
+        sys.stdout = StringIO.StringIO()
+        nothing()
+        captured = sys.stdout
+        sys.stdout = old_stdout
+        self.assertEquals(captured.getvalue(), 'before\nafter\n')
+        self.assert_(isinstance(noisy, types.FunctionType))
+        self.assert_(nothing.func_closure is not None)
+
+        # Good decorators
+        # write a decorator ``good_pointless`` that
+        # updates the wrapped ``__doc__`` and ``func_name``
+        # and just calls the function
+        # ================================
+        
+        @good_pointless
+        def nothing2():
+            """doc string"""
+            pass
+
+        self.assertEquals(nothing2.__doc__, 'doc string')
+        self.assertEquals(nothing2.func_name, 'nothing2')
+
+        # Parameterized Decorators
+        # write a decorator ``times_n`` that takes a
+        # number (n) and multiples the result of the
+        # function it is wrapping by that number (n).
+        # ================================
+        
+
+        @times_n(3)
+        def echo(txt):
+            return txt
+        
+        self.assertEquals(echo('f'), 'fff')
+        self.assertEquals(echo(4), 12)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
